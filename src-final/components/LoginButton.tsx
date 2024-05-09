d637961f51e236ae649d9c335ebebc938ca0335a
Cole Blender <coleblender@gmail.com>
2024-05-09 08:52:17 -0700

    add social icons

--- src-final/components/LoginButton.tsx
+++ src-final/components/LoginButton.tsx
@@ -5,6 +5,7 @@ import { Provider } from "@supabase/supabase-js";
 import { useRouter } from "next/navigation";
 import { useTransition } from "react";
 import toast from "react-hot-toast";
+import { GitHubIcon } from "./ui/social-icons";
 
 function LoginButton() {
   const router = useRouter();
@@ -24,10 +25,11 @@ function LoginButton() {
 
   return (
     <button
-      className="bg-black border-white border w-48 py-2 rounded-md hover:bg-emerald-950"
+      className="bg-black border-white border w-48 py-2 rounded-md hover:bg-emerald-950 flex items-center justify-center gap-2"
       onClick={() => handleClickLoginButton("github")}
       disabled={isPending}
     >
+      <GitHubIcon />
       {isPending ? "Logging in..." : "Login with GitHub"}
     </button>
   );
